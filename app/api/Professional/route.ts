af3b001c29e6670905d6096a5de91e4461680f37
Eric Kinuthia <mungaikinus@gmail.com>
2023-10-01 15:03:44 -0700

    removed comments

--- app/api/Professional/route.ts
+++ app/api/Professional/route.ts
@@ -1,39 +1,3 @@
-// pages/api/getHealthcareProfessional.ts
-
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import prisma from '@/app/libs/prismadb'; // Adjust the import to your prisma client location
-// import { NextResponse } from 'next/server';
-
-// interface Iparams {
-//     id: string;
-// }
-
-// export default async function GET(
-//     req: Request,
-//     { params }: { params: Iparams }) {
-//   if (req.method !== 'GET') {
-//     return NextResponse.error();
-//   }
-  
-//   const { id } = params;// Assume you pass userId as a query parameter
-  
-//   try {
-//     const healthcareProfessional = await prisma.healthcareProfessional.findFirst({
-//       where: { userId: id }
-//     });
-    
-//     if (!healthcareProfessional) {
-//       return NextResponse.error();
-//     }
-    
-//     return NextResponse.json(healthcareProfessional);
-//   } catch (error) {
-//     console.error('Error in getHealthcareProfessional:', error);
-//     return NextResponse.error();
-//   }
-// }
-
-// pages/api/checkHealthcareProfessional.ts
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/app/libs/prismadb'; // Adjust the import to your prisma client location
 import { NextResponse } from 'next/server';
@@ -44,7 +8,7 @@ export async function POST(
   req: Request,
    res: NextApiResponse) {
   if (req.method !== 'POST') {
-    return 
+    return ({ error: 'Method not allowed' });
   }
 
   const body = await req.json();
