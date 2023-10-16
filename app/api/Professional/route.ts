7f94d373c46355bf9caafc63d6af3364c3f1e37c
Eric Kinuthia <mungaikinus@gmail.com>
2023-10-15 23:27:18 -0700

    new api fix

--- app/api/Professional/route.ts
+++ app/api/Professional/route.ts
@@ -1,23 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/app/libs/prismadb'; // Adjust the import to your prisma client location
-import { NextResponse } from 'next/server';
 
-
-
-export async function POST(
-  req: Request,
-  res: Response,) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
-    return ({ error: 'Method not allowed' });
+    return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const body = await req.json();
+  const body = req.body;
   console.log(body);
-  const { id } = body;// Assume you pass userId as a query parameter
+  const { id } = body; 
 
-  
   const healthcareProfessional = await prisma.healthcareProfessional.findFirst({
-      where: { userId: id }
-    });
+    where: { userId: id }
+  });
   console.log(healthcareProfessional);
-  return Response.json(healthcareProfessional);
+  
+  res.json(healthcareProfessional);
 }
+
+
