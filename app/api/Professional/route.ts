590c685c2a367d8c1dc286e278f709c1d165e738
Eric Kinuthia <mungaikinus@gmail.com>
2023-10-15 23:30:26 -0700

    Request

--- app/api/Professional/route.ts
+++ app/api/Professional/route.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/app/libs/prismadb'; // Adjust the import to your prisma client location
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req:Request, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const body = req.body;
+  const body = await req.json();
   console.log(body);
   const { id } = body; 
 
