2ef0bd2707b957e9d5e98d3aec3e25cb602a3c5f
Eric Kinuthia <mungaikinus@gmail.com>
2023-10-17 10:37:31 -0700

    use client

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import { FaHeart, FaHandshake, FaSmile, FaClock, FaCheck, FaRegLightbulb, FaGlobeAmericas, FaUserFriends, FaThumbsUp, FaShieldAlt } from 'react-icons/fa';
 
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+"use client";
 import MainNav from "./main-nav";
 
 const Navbar: React.FC = () => {
